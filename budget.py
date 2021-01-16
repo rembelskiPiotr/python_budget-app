cf50fd3a1f3f7c47a8ad71a8b49a449dc2de70ef
rembelskiPiotr <rembelskipiotr92@gmail.com>
2021-01-16 19:08:57 +0100

    class finished

--- budget.py
+++ budget.py
@@ -1,14 +1,53 @@
 class Category:
-  ledger = []
-  name = ""
+  category_name = ""
 
   def __init__(self, input_name):
-    self.name = input_name
+    self.category_name = input_name
+    self.ledger = []
 
   def deposit(self, amount, description=""):
     self.ledger.append({"amount": amount, "description": description})
-  def 
+
+  def withdraw(self, amount, description=""):
+    if self.check_funds(amount):
+      self.deposit(-amount, description)
+      return True
+    else:
+      return False
+
+  def get_balance(self):
+    return sum_by_key(self.ledger, "amount")
+
+  def transfer(self, amount, budget_category):
+    if self.check_funds(amount):
+      self.withdraw(amount,"Transfer to " + budget_category.category_name)
+      budget_category.deposit(amount, "Transfer from " + self.category_name)
+      return True
+    else:
+      return False
+    
+  def check_funds(self, amount):
+    if amount > self.get_balance():
+      return False
+    else:
+      return True
+  
+  def __str__(self):
+    printout = self.category_name.center(30,"*") + "\n"
+    for element in self.ledger:
+      printout += element["description"][:23].ljust(23)
+      printout += str("{:.2f}".format(element["amount"]).rjust(7)) + "\n"
+    printout += "Total: " + str(self.get_balance())
+    return printout
 
 
 
 def create_spend_chart(categories):
+  
+
+def sum_by_key(list, key):
+  result = 0
+  for element in list:
+    result += element[key]
+  return result
+  
